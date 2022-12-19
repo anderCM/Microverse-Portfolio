b27c6dc3ca1a42856ecdc6f9eae684664c7624a4
anderCM2 <ginoander_5@hotmail.com>
2022-12-19 16:32:58 -0500

    Hamburger menu onclick added

---
+++ index.js
@@ -0,0 +1,32 @@
+const hamburger = document.querySelector(".humburger-container");
+
+let controller = false;
+hamburger.addEventListener("click", function () {
+  const navbar = document.querySelector(".menu-container");
+  const links = navbar.querySelectorAll("a");
+  const slice1 = document.querySelector(".slice-1");
+  const slice2 = document.querySelector(".slice-2");
+  const meat = document.querySelector(".meat");
+  if (!controller) {
+    navbar.classList.add("full-screen-menu");
+    this.classList.add("full-screen-hamburger");
+    links.forEach((link) => {
+      link.classList.add("full-screen-menu-links");
+    });
+    slice1.classList.add("full-screen-slice1");
+    slice2.classList.add("full-screen-slice2");
+    meat.classList.add("full-screen-meat");
+
+    controller = true;
+    return;
+  }
+  navbar.classList.remove("full-screen-menu");
+  this.classList.remove("full-screen-hamburger");
+  links.forEach((link) => {
+    link.classList.remove("full-screen-menu-links");
+  });
+  slice1.classList.remove("full-screen-slice1");
+  slice2.classList.remove("full-screen-slice2");
+  meat.classList.remove("full-screen-meat");
+  controller = false;
+});
